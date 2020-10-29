ed0cc8d5bd3111b3248b2dc5ac5a82aebcab3acf
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-29 16:23:10 -0700

    added ability to use JWT to client

--- Models/ApiHelper.cs
+++ Models/ApiHelper.cs
@@ -7,24 +7,27 @@ namespace TravelClient.Models
   {
     public static async Task<string> GetAll()
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:4005/api");
       RestRequest request = new RestRequest($"Reviews", Method.GET);
+      request.AddHeader("Authorization", $"Bearer {EnvironmentVariables.ApiKey}");
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
 
     public static async Task<string> Get(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:4005/api");
       RestRequest request = new RestRequest($"Reviews/{id}", Method.GET);
+      request.AddHeader("Authorization", $"Bearer {EnvironmentVariables.ApiKey}");
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
 
     public static async Task Post(string newReview)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:4005/api");
       RestRequest request = new RestRequest($"Reviews", Method.POST);
+      request.AddHeader("Authorization", $"Bearer {EnvironmentVariables.ApiKey}");
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newReview);
       var response = await client.ExecuteTaskAsync(request);
@@ -32,8 +35,9 @@ namespace TravelClient.Models
 
     public static async Task Put(int id, string newReview)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:4005/api");
       RestRequest request = new RestRequest($"Reviews/{id}", Method.PUT);
+      request.AddHeader("Authorization", $"Bearer {EnvironmentVariables.ApiKey}");
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newReview);
       var response = await client.ExecuteTaskAsync(request);
@@ -41,24 +45,27 @@ namespace TravelClient.Models
 
     public static async Task Delete(int id)
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:4005/api");
       RestRequest request = new RestRequest($"Reviews/{id}", Method.DELETE);
+      request.AddHeader("Authorization", $"Bearer {EnvironmentVariables.ApiKey}");
       request.AddHeader("Content-Type", "application/json");
       var response = await client.ExecuteTaskAsync(request);
     }
 
     public static async Task<string> GetPopular()
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:4005/api");
       RestRequest request = new RestRequest($"Reviews/popular", Method.GET);
+      request.AddHeader("Authorization", $"Bearer {EnvironmentVariables.ApiKey}");
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
 
     public static async Task<string> GetRandom()
     {
-      RestClient client = new RestClient("http://localhost:5000/api");
+      RestClient client = new RestClient("http://localhost:4005/api");
       RestRequest request = new RestRequest($"Reviews/random", Method.GET);
+      request.AddHeader("Authorization", $"Bearer {EnvironmentVariables.ApiKey}");
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
---
+++ Models/EnvironmentVariables.cs
@@ -0,0 +1,7 @@
+namespace TravelClient.Models
+{
+     public static class EnvironmentVariables
+    {
+        public static string ApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJuYmYiOjE2MDM5MTg0MDYsImV4cCI6MTYwNDUyMzIwNiwiaWF0IjoxNjAzOTE4NDA2fQ.J9QVYXTlwqrH90Oh-YBh8EQI29o_tDG5V9j7sfSa8Ig";
+    }
+}
